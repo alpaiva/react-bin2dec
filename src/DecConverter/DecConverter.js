f6096b629a199008994f33cd536fd93782bc5fd2
Andre Paiva <and.l.paiva@gmail.com>
2024-11-15 18:35:20 -0300

    Adjust in input text - binary value

--- src/DecConverter/DecConverter.js
+++ src/DecConverter/DecConverter.js
@@ -8,8 +8,10 @@ const DevConverter = () => {
     const [decimal, setDecimal] = useState('')
     const [binary, setBinary] = useState('')
 
-    const getDecimalValue = (e) => {
-        setDecimal(binToDec(e.target.value))
+    const handleNumbers = (e) => {
+        let value = e.target.validity.valid ? e.target.value : binary
+        setBinary(value)
+        setDecimal(binToDec(value))
     }
 
     const clear = () => {
@@ -22,12 +24,13 @@ const DevConverter = () => {
         <div className='main-group'>
             <div className='binaryGroup'>
                 <label htmlFor='binaryNumber' >Binary Number</label>
-                <input id='binaryNumber' placeholder='binary' onChange={getDecimalValue} />
+                <input id='binaryNumber' placeholder='binary' type="text" pattern="[0-1]*" onChange={handleNumbers} value={binary}/>
             </div>
             <div className='decimalGroup'>
                 <label htmlFor='decimalNumber' >Decimal Number</label>
                 <input id='decimalNumber' readOnly value={decimal} />
             </div>
+            <button onClick={clear}>Clear</button>
         </div>
     )
 
